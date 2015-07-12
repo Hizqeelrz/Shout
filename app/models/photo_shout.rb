16aa453d80964b27bae56142519526b2620e7e25
Hizqeel Raza <hizqeel1214@gmail.com>
2015-07-12 18:58:06 +0500

    relationship added

--- app/models/photo_shout.rb
+++ app/models/photo_shout.rb
@@ -1,9 +1,14 @@
 class PhotoShout < ActiveRecord::Base
 
-	has_attached_file :image, styles: {
-		shout: "200x200>"
-	}
+	# has_attached_file :image, styles: {
+	# 	shout: "200x200>"
+	# }
 
 	# validates_attachment_content_type :attachment, :content_type => %w(image/jpeg image/jpg)
+
+
+	has_attached_file :image , :styles => { :medium => "3000x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+      validates_attachment_content_type :image  , :content_type => /\Aimage\/.*\Z/
+
 
 end
