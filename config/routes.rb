b801340f81faa73e708306a02a6f6f306371d455
Hizqeel Raza <hizqeel1214@gmail.com>
2015-07-13 17:37:24 +0500

    following files created

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,9 @@ Rails.application.routes.draw do
   root to: 'homes#show', via: :get
   resource :dashboard, only: [:show]
   resource :session, only: [:new, :create, :destroy]
-  resources :users, only: [:new, :create, :show] do
+  resources :users, only: [:index, :new, :create, :show] do
     post 'follow' => 'following_relationships#create'
+    delete 'follow' => 'following_relationships#destroy'
   end
   resources :shouts, only: [:show, :destroy]
   resources :text_shouts, only: [:create]
