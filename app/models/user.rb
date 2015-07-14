427d39739b96c9c876677b1f90010e4486425dc7
Hizqeel Raza <hizqeel1214@gmail.com>
2015-07-15 02:31:25 +0500

    updated model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,32 +2,31 @@ class User < ActiveRecord::Base
 
 	has_many :shouts
 
-	has_many :followed_user_relationships,
-						foreign_key: :follower_id,
-						class_name: 'FollowingRelationship'
-	has_many :followed_users, through: :followed_user_relationships
+  has_many :followed_user_relationships,
+        foreign_key: :follower_id,
+        class_name: 'FollowingRelationship'
+  has_many :followed_users, through: :followed_user_relationships
 
-	has_many :follower_relationships,
-						foreign_key: :followed_user_id,
-						class_name: 'FollowingRelationship'
-	has_many :followers, through: :follower_relationships
+  has_many :follower_relationships,
+            foreign_key: :followed_user_id,
+            class_name: 'FollowingRelationship'
+  has_many :followers, through: :follower_relationships
 
 
   validates :email, presence: true, uniqueness: true
   validates :password_digest, presence: true
 
 
-
   def following? user
-  	followed_user_ids.include? user.id
+    followed_user_ids.include? user.id
   end
 
   def follow user
-  	followed_users << user
+    followed_users << user
   end
 
   def unfollow user
-  	followed_users.delete(user)
+    followed_users.delete(user)
   end
 
 end
